51adf31adaa2c7a420d66203c7440f88f09ef83b
Josef Varela <josef.var@gmail.com>
2018-02-25 16:40:17 +0100

    Add files via upload

---
+++ Ejercicios Semana 5/Ebay/Ebay/Data/EbayContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ebay.Models
+{
+    public class EbayContext : DbContext
+    {
+        public EbayContext (DbContextOptions<EbayContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ebay.Models.Product> Product { get; set; }
+    }
+}
---
+++ Ejercicios Semana 5/Ebay/Ebay/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ebay.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        [Range(1,5)]
+        public int Stars { get; set; }
+        public bool Offer { get; set; }
+    }
+}
